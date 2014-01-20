adc02ac90470147badbda34f749acdbe61bc509d
Yanxing Wang <demiwangya@gmail.com>
2014-01-20 15:49:52 -0800

    download and upload of GLTextureDrv

--- src/demi/drvGL/GLTexture.cpp
+++ src/demi/drvGL/GLTexture.cpp
@@ -137,9 +137,12 @@ namespace Demi
 
     void* DiGLTextureDrv::LockLevel(uint32 level, uint32 surface, DiLockFlag lockflag)
     {
-        if (!mLockedBuffer)
+        AllocateBuffer();
+
+        if (lockflag != LOCK_DISCARD)
         {
-            mLockedBuffer = DI_NEW uint8[mImageSize];
+            //Download();
+            // TODO
         }
 
         return mLockedBuffer;
@@ -147,37 +150,7 @@ namespace Demi
 
     void DiGLTextureDrv::UnlockLevel(uint32 level, uint32 surface)
     {
-        glBindTexture(mGLTextureType, mTextureID);
-
-        DiPixelFormat fmt = mParent->GetFormat();
-        BOOL isCompressed = DiPixelBox::IsCompressedFormat(fmt);
-
-        if (isCompressed)
-        {
-            /*switch(mGLTextureType)
-            {
-            case GL_TEXTURE_2D:
-            case GL_TEXTURE_CUBE_MAP:
-            glCompressedTexImage2DARB(mFaceTarget, level,
-            mGLFormat,
-            dest.getWidth(),
-            dest.getHeight(),
-            0,
-            data.getConsecutiveSize(),
-            data.data);
-            break;
-            }*/
-        }
-        else
-        {
-
-        }
-
-        if (mParent->GetResourceUsage() != RU_STATIC)
-        {
-            DI_DELETE[] mLockedBuffer;
-            mLockedBuffer = nullptr;
-        }
+        // TODO
     }
 
     void* DiGLTextureDrv::GetSurfaceHandle()
@@ -187,6 +160,140 @@ namespace Demi
 
     void DiGLTextureDrv::CopyFromMemory(const DiPixelBox &srcBox, const DiBox &dst, uint32 level, uint32 surface /*= 0*/)
     {
+        AllocateBuffer();
+        Upload(srcBox, dst, level, surface);
+        DeallocateBuffer();
+    }
 
+    void DiGLTextureDrv::AllocateBuffer()
+    {
+        if (!mLockedBuffer)
+        {
+            mLockedBuffer = DI_NEW uint8[mImageSize];
+        }
+    }
+
+    void DiGLTextureDrv::DeallocateBuffer()
+    {
+        if (mParent->GetResourceUsage() & RU_STATIC)
+        {
+            delete[](uint8*)mLockedBuffer;
+            mLockedBuffer = 0;
+        }
+    }
+
+    void DiGLTextureDrv::Upload(const DiPixelBox &src, const DiBox &dst, uint32 level, uint32 surface)
+    {
+        glBindTexture(mGLTextureType, mTextureID);
+        
+        DiPixelFormat fmt = mParent->GetFormat();
+        BOOL isCompressed = DiPixelBox::IsCompressedFormat(fmt);
+
+        GLenum faceType = GL_TEXTURE_2D;
+        if (mGLTextureType == GL_TEXTURE_CUBE_MAP)
+            faceType = GL_TEXTURE_CUBE_MAP_POSITIVE_X + surface;
+
+        if (isCompressed)
+        {
+            switch (mGLTextureType)
+            {
+            case GL_TEXTURE_2D:
+            case GL_TEXTURE_CUBE_MAP:
+                if (dst.left == 0 && dst.top == 0)
+                {
+                    glCompressedTexImage2DARB(faceType, level,
+                        mGLFormat,
+                        dst.GetWidth(),
+                        dst.GetHeight(),
+                        0,
+                        src.GetConsecutiveSize(),
+                        src.data);
+                }
+                else
+                {
+                    glCompressedTexSubImage2DARB(faceType, level,
+                        dst.left, dst.top,
+                        dst.GetWidth(), dst.GetHeight(),
+                        mGLFormat, src.GetConsecutiveSize(),
+                        src.data);
+                }
+                break;
+            }
+        }
+        else
+        {
+            if (src.GetWidth() != src.rowPitch)
+                glPixelStorei(GL_UNPACK_ROW_LENGTH, src.rowPitch);
+            if (src.GetWidth() > 0 && src.GetHeight()*src.GetWidth() != src.slicePitch)
+                glPixelStorei(GL_UNPACK_IMAGE_HEIGHT, (src.slicePitch / src.GetWidth()));
+            if (src.left > 0 || src.top > 0)
+                glPixelStorei(GL_UNPACK_SKIP_PIXELS, src.left + src.rowPitch * src.top);
+            if ((src.GetWidth()*DiPixelBox::GetNumElemBytes(src.format)) & 3) {
+                // Standard alignment of 4 is not right
+                glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+            }
+            switch (mGLTextureType) 
+            {
+            case GL_TEXTURE_2D:
+            case GL_TEXTURE_CUBE_MAP:
+                glTexSubImage2D(faceType, level,
+                    dst.left, dst.top,
+                    dst.GetWidth(), dst.GetHeight(),
+                    mGLFormat, mGLFormat,
+                    src.data);
+                break;
+            }
+        }
+
+        // restore
+        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+        if (GLEW_VERSION_1_2)
+            glPixelStorei(GL_UNPACK_IMAGE_HEIGHT, 0);
+        glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0);
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+    }
+
+    void DiGLTextureDrv::Download(const DiPixelBox &data, uint32 level, uint32 surface)
+    {
+        if (data.GetWidth() != mParent->GetWidth() ||
+            data.GetHeight() != mParent->GetHeight())
+        {
+            DI_WARNING("Only download of entire buffer of the texture is supported.");
+            return;
+        }
+
+        glBindTexture(mGLTextureType, mTextureID);
+
+        DiPixelFormat fmt = mParent->GetFormat();
+        BOOL isCompressed = DiPixelBox::IsCompressedFormat(fmt);
+
+        GLenum faceType = GL_TEXTURE_2D;
+        if (mGLTextureType == GL_TEXTURE_CUBE_MAP)
+            faceType = GL_TEXTURE_CUBE_MAP_POSITIVE_X + surface;
+        
+        if (isCompressed)
+        {
+            glGetCompressedTexImageARB(faceType, level, data.data);
+        }
+        else
+        {
+            if (data.GetWidth() != data.rowPitch)
+                glPixelStorei(GL_PACK_ROW_LENGTH, data.rowPitch);
+            if (data.GetHeight()*data.GetWidth() != data.slicePitch)
+                glPixelStorei(GL_PACK_IMAGE_HEIGHT, (data.slicePitch / data.GetWidth()));
+            if (data.left > 0 || data.top > 0)
+                glPixelStorei(GL_PACK_SKIP_PIXELS, data.left + data.rowPitch * data.top);
+            if ((data.GetWidth()*DiPixelBox::GetNumElemBytes(data.format)) & 3) {
+                // Standard alignment of 4 is not right
+                glPixelStorei(GL_PACK_ALIGNMENT, 1);
+            }
+            // We can only get the entire texture
+            glGetTexImage(faceType, level, mGLFormat, mGLFormat, data.data);
+            // Restore defaults
+            glPixelStorei(GL_PACK_ROW_LENGTH, 0);
+            glPixelStorei(GL_PACK_IMAGE_HEIGHT, 0);
+            glPixelStorei(GL_PACK_SKIP_PIXELS, 0);
+            glPixelStorei(GL_PACK_ALIGNMENT, 4);
+        }
     }
 }
