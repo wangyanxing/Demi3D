f2728afa31ba7ec51ca68dde6c68fda048bf03d6
Yanxing Wang <demiwangya@gmail.com>
2014-01-20 17:03:11 -0800

    new files for RT/DepthBuf

---
+++ src/demi/drvGL/GLRenderTarget.h
@@ -0,0 +1,40 @@
+
+/********************************************************************
+    File:       GLRenderTarget.h
+    Creator:    demiwangya
+*********************************************************************/
+
+#pragma once
+
+#include "RenderTarget.h"
+
+#define GL_DEPTH24_STENCIL8_EXT 0x88F0
+
+namespace Demi
+{
+
+    class DI_GLDRV_API DiGLRenderTarget : public DiRenderTarget
+    {
+    public:
+
+        DiGLRenderTarget();
+
+        virtual     ~DiGLRenderTarget();
+
+    public:
+
+        void            AttachSurface();
+
+        void            DetachSurface();
+
+        bool            BindRenderTarget(uint8 mrtid);
+
+        bool            BindDepthStencil();
+
+        bool            IsCompatibleWith(DiDepthBuffer* db);
+
+        DiDepthBuffer*  CreateDepthBuffer();
+
+        void            PreBind();
+    };
+}
