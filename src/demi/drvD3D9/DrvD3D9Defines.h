33e8d91d9f20eebd25e50fd67eb1dcae4b0a9895
Yanxing Wang <demiwangya@gmail.com>
2014-01-28 23:15:43 -0800

    export defines

--- src/demi/drvD3D9/DrvD3D9Defines.h
+++ src/demi/drvD3D9/DrvD3D9Defines.h
@@ -4,14 +4,17 @@
     Creator:    demiwangya
 *********************************************************************/
 
-#pragma once
+#ifndef DrvD3D9Defines_h__
+#define DrvD3D9Defines_h__
 
 #ifdef DEMI_STATIC_API
 #    define DI_D3D9DRV_API
 #else
 #    ifdef DI_D3D9_EXPORT
-#        define DI_D3D9DRV_API  __declspec(dllexport)
+#        define DI_D3D9DRV_API __declspec(dllexport)
 #    else
 #        define DI_D3D9DRV_API __declspec(dllimport)
 #    endif
 #endif
+
+#endif // DrvD3D9Defines_h__
