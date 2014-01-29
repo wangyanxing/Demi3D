5cebeea4ffa4ac33b0ce27bf1df66cd74c10147c
Yanxing Wang <demiwangya@gmail.com>
2014-01-28 23:30:43 -0800

    file archive changes for osx

--- src/demi/gfx/FileArchive.cpp
+++ src/demi/gfx/FileArchive.cpp
@@ -6,18 +6,167 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-#include <direct.h>
-#include <io.h>
-#include <Shlwapi.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+
+#if DEMI_PLATFORM == DEMI_PLATFORM_WIN32
+#   include <direct.h>
+#   include <io.h>
+#   include <Shlwapi.h>
+#else
+#   include <sys/params.h>
+#   include <dirent.h>
+#   include <unistd.h>
+#   include <fnmatch.h>
+
+struct _finddata_t
+{
+    char *name;
+    int attrib;
+    unsigned long size;
+};
+
+struct _find_search_t
+{
+    char *pattern;
+    char *curfn;
+    char *directory;
+    int dirlen;
+    DIR *dirfd;
+};
+
+#define _A_NORMAL 0x00  /* Normalfile-Noread/writerestrictions */
+#define _A_RDONLY 0x01  /* Read only file */
+#define _A_HIDDEN 0x02  /* Hidden file */
+#define _A_SYSTEM 0x04  /* System file */
+#define _A_ARCH   0x20  /* Archive file */
+#define _A_SUBDIR 0x10  /* Subdirectory */
+
+
+intptr_t _findfirst(const char *pattern, struct _finddata_t *data)
+{
+    _find_search_t *fs = new _find_search_t;
+    fs->curfn = NULL;
+    fs->pattern = NULL;
+
+    // Separate the mask from directory name
+    const char *mask = strrchr(pattern, '/');
+    if (mask)
+    {
+        fs->dirlen = mask - pattern;
+        mask++;
+        fs->directory = (char *)malloc(fs->dirlen + 1);
+        memcpy(fs->directory, pattern, fs->dirlen);
+        fs->directory[fs->dirlen] = 0;
+    }
+    else
+    {
+        mask = pattern;
+        fs->directory = strdup(".");
+        fs->dirlen = 1;
+    }
+
+    fs->dirfd = opendir(fs->directory);
+    if (!fs->dirfd)
+    {
+        _findclose((intptr_t)fs);
+        return -1;
+    }
+
+    /* Hack for "*.*" -> "*' from DOS/Windows */
+    if (strcmp(mask, "*.*") == 0)
+        mask += 2;
+    fs->pattern = strdup(mask);
+
+    /* Get the first entry */
+    if (_findnext((intptr_t)fs, data) < 0)
+    {
+        _findclose((intptr_t)fs);
+        return -1;
+    }
+
+    return (intptr_t)fs;
+}
+
+int _findnext(intptr_t id, struct _finddata_t *data)
+{
+    _find_search_t *fs = (_find_search_t *)id;
+
+    /* Loop until we run out of entries or find the next one */
+    dirent *entry;
+    for (;;)
+    {
+        if (!(entry = readdir(fs->dirfd)))
+            return -1;
+
+        /* See if the filename matches our pattern */
+        if (fnmatch(fs->pattern, entry->d_name, 0) == 0)
+            break;
+    }
+
+    if (fs->curfn)
+        free(fs->curfn);
+    data->name = fs->curfn = strdup(entry->d_name);
+
+    size_t namelen = strlen(entry->d_name);
+    char *xfn = new char[fs->dirlen + 1 + namelen + 1];
+    sprintf(xfn, "%s/%s", fs->directory, entry->d_name);
+
+    /* stat the file to get if it's a subdir and to find its length */
+    struct stat stat_buf;
+    if (stat(xfn, &stat_buf))
+    {
+        // Hmm strange, imitate a zero-length file then
+        data->attrib = _A_NORMAL;
+        data->size = 0;
+    }
+    else
+    {
+        if (S_ISDIR(stat_buf.st_mode))
+            data->attrib = _A_SUBDIR;
+        else
+            /* Default type to a normal file */
+            data->attrib = _A_NORMAL;
+
+        data->size = (unsigned long)stat_buf.st_size;
+    }
+
+    delete[] xfn;
+
+    /* Files starting with a dot are hidden files in Unix */
+    if (data->name[0] == '.')
+        data->attrib |= _A_HIDDEN;
+
+    return 0;
+}
+
+int _findclose(intptr_t id)
+{
+    int ret;
+    _find_search_t *fs = (_find_search_t *)id;
+
+    ret = fs->dirfd ? closedir(fs->dirfd) : 0;
+    free(fs->pattern);
+    free(fs->directory);
+    if (fs->curfn)
+        free(fs->curfn);
+    delete fs;
+
+    return ret;
+}
+
+#endif
 
 namespace Demi 
 {
     static bool is_absolute_path(const char* path)
     {
+#if DEMI_PLATFORM == DEMI_PLATFORM_WIN32
         if (isalpha(uint8(path[0])) && path[1] == ':')
             return true;
+#endif
         return path[0] == '/' || path[0] == '\\';
     }
 
