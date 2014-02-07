49c9edb3e1f7b4fe1ad35b4d44f352b273879af6
Yanxing Wang <demiwangya@gmail.com>
2014-02-07 10:15:27 -0800

    gl rt bug

--- src/demi/drvGL/GLRenderTarget.cpp
+++ src/demi/drvGL/GLRenderTarget.cpp
@@ -80,10 +80,8 @@ namespace Demi
             sb = DI_NEW DiGLRenderBuffer(stencilFormat, GetWidth(), GetHeight());
         }
 
-        ret = DI_NEW DiGLDepthBuffer(0, GetWidth(), GetHeight(),
-            static_cast<DiGLDriver*>(Driver)->GetCurrentContext(), db, sb, 0, 0, false);
-
-        return ret;
+        auto context = static_cast<DiGLDriver*>(Driver)->GetCurrentContext();
+        return DI_NEW DiGLDepthBuffer(0, GetWidth(), GetHeight(), context, db, sb, 0, 0, false);
     }
 
     void DiGLRenderTarget::PreBind()
