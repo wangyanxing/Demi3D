2566c169547f3c4e718145478bf2a531ef342515
Yanxing Wang <demiwangya@gmail.com>
2014-02-14 14:37:57 -0800

    csm params

--- src/demi/gfx/Light.h
+++ src/demi/gfx/Light.h
@@ -37,6 +37,62 @@ namespace Demi
         MAX_LIGHT_TYPES
     };
     
+#define MAX_CASCADE_SPLITS 4
+    
+    /** CSM parameters
+     */
+    struct DI_GFX_API CascadeParams
+    {
+        /** Simply do nothing
+         */
+        CascadeParams()
+        {
+        }
+        
+        /** Set the splits and fade params
+         */
+        void Set(float split1, float split2, float split3, float split4,
+                          float fadeStart, float biasAutoAdjust = 1.0f) :
+        
+            fadeStart(fadeStart),
+            biasAutoAdjust(biasAutoAdjust)
+        {
+            splits[0] = split1;
+            splits[1] = split2;
+            splits[2] = split3;
+            splits[3] = split4;
+        }
+        
+        /** Validate parameters.
+         */
+        void Validate()
+        {
+            for (unsigned i = 0; i < MAX_CASCADE_SPLITS; ++i)
+                splits_[i] = DiMax(splits[i], 0.0f);
+            fadeStart_ = DiMath::Clamp(fadeStart, std::numeric_limits<float>::epsilon(), 1.0f);
+        }
+        
+        /** Return shadow maximum range.
+         */
+        float GetShadowRange() const
+        {
+            float ret = 0.0f;
+            for (unsigned i = 0; i < MAX_CASCADE_SPLITS; ++i)
+                ret = DiMath::Max(ret, splits[i]);
+            
+            return ret;
+        }
+        
+        /// Far clip values of the splits.
+        float splits[4];
+        
+        /// The point relative to the total shadow range where shadow fade begins (0.0 - 1.0)
+        float fadeStart;
+        
+        /// Automatic depth bias adjustment strength.
+        float biasAutoAdjust;
+    };
+    
     /** A dynamic light source created by the scene manager
         lights should be attached to nodes in order to get
         the positions even full transformations
@@ -64,6 +120,8 @@ namespace Demi
         void            SetColor(const DiColor &color) { mColor = color; }
         
         DiString&       GetType();
+        
+        CascadeParams&  GetCSMParams() { return mCascadeParams; }
 
     protected:
 
@@ -75,6 +133,9 @@ namespace Demi
 
         /// The relative camera regarding the shadow processing
         DiCamera*       mShadowCamera;
+        
+        /// for cascade shadow mapping
+        CascadeParams   mCascadeParams;
     };
 }
 
